03827606e512a0dd1cce73d25ab7b5b37460db1d
Mariam Qasim <mariam.a.qasim@gmail.com>
2016-08-29 17:33:42 -0400

    second commit, couldnt get lodash to work, so couldnt test its methods

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,3 +1,32 @@
 var $ = require('jquery');
+// var _ = require("lodash");
+require("style!css!../css/dice.css");
 
-$('#root').html('<h3>Hello World!</h3>')
+
+var array = [1, 2, 3, 4, 5, 6];
+// var number = _.shuffle(array);
+
+
+module.exports =
+ $("#button").on('click', function(e){
+  array.map(function(n){
+
+    $("#greeting").html("The dice has been rolled, the number you got is: " + n);
+      });
+
+  var again = $(this).html("Play again?");
+
+  $(again).on('click', function(x){
+
+    $(this).html('Roll Dice!');
+
+  })
+
+  // try for lodash - _.shuffle(n)?
+
+//   $(this).html("Play again?").on('click', function(playagain){
+//
+// $(this).html('Roll dice');!
+//
+//   })
+});
